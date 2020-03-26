b22f3c594cbd49c48b67f2e7ec7052cbd46a4b42
Pierre Vieira <49538805+PierreVieira@users.noreply.github.com>
2020-03-26 20:25:14 -0300

    [+] if/else if/else and switch

---
+++ 04 - Estruturas de Controle/057 - Usando a Estrutura IF & ELSE IF & ELSE.js
@@ -0,0 +1,24 @@
+Number.prototype.entre = function (inicio, fim) {
+    return this >= inicio && this <= fim
+}
+
+const imprimir_resultado = function(nota){
+    if (nota.entre(9, 10)){
+        console.log('Quadro de Honra')
+    } else if (nota.entre(7, 8.99)){
+        console.log('Aprovado')
+    } else if (nota.entre(4, 6.99)){
+        console.log('Recuperação')
+    } else if (nota.entre(0, 3.99)){
+        console.log('Reprovado')
+    } else {
+        console.log('Nota inválida!')
+    }
+    console.log('fim')
+}
+imprimir_resultado(10)
+imprimir_resultado(8.9)
+imprimir_resultado(6.55)
+imprimir_resultado(2.3)
+imprimir_resultado(-1)
+imprimir_resultado(11)
