9bff3fc6846584c4a622a6e74a3014a3baa38f91
Pierre Vieira <49538805+PierreVieira@users.noreply.github.com>
2020-04-11 19:57:46 -0300

    Update and rename 062 - Usando a Estrutura FOR IN.js to 062 - Usando a Estrutura FOR IN e FOR OF.js

--- 04 - Estruturas de Controle/062 - Usando a Estrutura FOR IN e FOR OF.js
+++ 04 - Estruturas de Controle/062 - Usando a Estrutura FOR IN e FOR OF.js
@@ -11,3 +11,13 @@ const pessoa = {
 for (let atributo in pessoa){
     console.log(`${atributo} = ${pessoa.atributo}`)
 }
+
+function teste(stirng){
+    let valores = stirng.split(' ').map(x => parseFloat(x))
+    console.log(valores)
+    for (let v of valores){
+        console.log(v)
+    }
+}
+
+teste('10 20 20 8 25 3 0 30 1')
